b064c1d2bd6a62f94461c087cda5568cc31cd4ee
Lyn.47 (a.k.a vhn-47) <viperred9867@gmail.com>
2026-04-19 10:41:59 +0700

    Vite Ngasus Kamu ya)

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PRJ-Portofolio-02/', // <-- Tambahkan baris ini, pastikan namanya sama persis
 })
